a3cb20f39f3c1884593d1ec9b94d33b7d90764c9
luisberretta <luisberretta35@gmail.com>
2020-12-17 12:26:00 -0300

    Creación de header y footer con tipografía.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './componentes/layout/header/header.component';
+import { FooterComponent } from './componentes/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
