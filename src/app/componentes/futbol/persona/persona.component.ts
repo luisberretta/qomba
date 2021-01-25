5179c587ac625b14c85377ce65990b1c624eb5b3
Luis <luisberretta35@gmail.com>
2021-01-25 17:47:01 -0300

    Hago web responsive.

--- src/app/componentes/futbol/persona/persona.component.ts
+++ src/app/componentes/futbol/persona/persona.component.ts
@@ -17,11 +17,12 @@ import {
 })
 export class PersonaComponent implements OnInit, OnChanges {
 
-  color: string = "blue";
   @Input() colorPartes: any;
   @Input() partesSvg: any;
   @ViewChildren('path') paths: QueryList<any>;
   @ViewChild('dataContainer') dataContainer: ElementRef;
+  @ViewChildren('dataGroup') dataGroup: ElementRef;
+  color: string;
 
   constructor(public renderer : Renderer2) { }
 
@@ -34,16 +35,27 @@ export class PersonaComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(changeRecord: SimpleChanges): void {
-    if(changeRecord.colorPartes && changeRecord.colorPartes.currentValue) {
-      this.paths.forEach((path) => {
-        let idParte = path.nativeElement.attributes['data-idparte'].value;
-        let idParteColor = changeRecord.colorPartes.currentValue.idParte;
-        let color = changeRecord.colorPartes.currentValue.color;
-        if(idParte == idParteColor) {
-          this.renderer.setAttribute(path.nativeElement, 'fill', color);
-        }
-      });
-    }
+
+  }
+
+  cambiarColor(event) {
+    console.log(event);
+    // this.renderer.setAttribute(this.dataGroup.nativeElement, 'fill', "red");
+    // if(booleano == "parte1") {
+    //   document.getElementById('grupo-1').setAttribute('fill', red);
+    // } else if(booleano == "mangas") {
+    //   document.getElementById('grupo-2').setAttribute('fill', 'green')
+    // } else {
+    //   document.getElementById('grupo-3').setAttribute('fill', 'yellow')
+    // }
+  }
+
+  aumentarZoom() {
+    document.getElementById('grupo-1').classList.add("zoom");
+  }
+
+  quitarZoom() {
+    document.getElementById('grupo-1').classList.remove("zoom");
   }
 
 
