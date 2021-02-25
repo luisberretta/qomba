032e641986c9bae0f5b5eee800d6a51cf4c97b1d
Luis <luisberretta35@gmail.com>
2021-02-25 13:30:46 -0300

    Fix id partes y subo colores modelo matador.

--- src/app/clases/ColorParte.ts
+++ src/app/clases/ColorParte.ts
@@ -1038,7 +1038,110 @@ export const coloresParte = [
   },
   {
     idModelo: 11,
-    partes:[],
+    partes:[
+      {
+        idParte: "Remera_principal",
+        nombreMostrar: "Remera principal",
+        colores: [
+          "#DB0606", "#F012B3", "#000000", "#DC12F0",
+          "#3A243B", "#4B0082", "#050FDF", "#ADD8E6",
+          "#00FFFF", "#008080", "#00FF00", "#90EE90",
+          "#BFFF00", "#FFFF00", "#FFBF00", "#FFA500",
+          "#FF8C00", "#964B00", "#696969", "#6699CC"],
+        posicionMatrix: {}
+      },
+      {
+        idParte: "Remera_estampa",
+        nombreMostrar: "Estampa",
+        colores: ["#DB0606", "#F012B3", "#000000", "#DC12F0",
+          "#3A243B", "#4B0082", "#050FDF", "#ADD8E6",
+          "#00FFFF", "#008080", "#00FF00", "#90EE90",
+          "#BFFF00", "#FFFF00", "#FFBF00", "#FFA500",
+          "#FF8C00", "#964B00", "#696969", "#6699CC"],
+        posicionMatrix: {}
+      },
+      {
+        idParte: "Remera_cuello_y_puños",
+        nombreMostrar: "Cuello y puños",
+        colores: ["#DB0606", "#050FDF", "#000000", "#FFFFFF"],
+        posicionMatrix: {}
+      },
+      {
+        idParte: "Medias_principal",
+        nombreMostrar: "Medias principal",
+        colores: ["#DB0606", "#050FDF", "#000000", "#FFFFFF"],
+        posicionMatrix: {},
+      },
+      {
+        idParte: "Medias_linea",
+        nombreMostrar: "Medias línea",
+        colores: ["#DB0606", "#050FDF", "#000000", "#FFFFFF"],
+        posicionMatrix: {}
+      },
+      {
+        idParte: "Short_principal",
+        nombreMostrar: "Short principal",
+        colores: ["#DB0606", "#050FDF", "#000000", "#FFFFFF"],
+        posicionMatrix: {}
+      },
+      {
+        idParte: "Short_linea_superior",
+        nombreMostrar: "Short línea superior",
+        colores: ["#DB0606", "#050FDF", "#000000", "#FFFFFF"],
+        posicionMatrix: {}
+      },
+      {
+        idParte: "Short_linea_inferior",
+        nombreMostrar: "Short línea inferior",
+        colores: ["#DB0606", "#050FDF", "#000000", "#FFFFFF"],
+        posicionMatrix: {}
+      },
+      {
+        idParte: "Short_linea_lateral",
+        nombreMostrar: "Short línea letaral",
+        colores: ["#DB0606", "#050FDF", "#000000", "#FFFFFF"],
+        posicionMatrix: {}
+      },
+      {
+        idParte: "Nombre",
+        nombreMostrar: "Nombre",
+        colores: [],
+        posicionMatrix: {}
+      },
+      {
+        idParte: "Número_espalda",
+        nombreMostrar: "Número espalda",
+        colores: [],
+        posicionMatrix: {}
+      },
+      {
+        idParte: "Número_delantero",
+        nombreMostrar: "Número delantero",
+        colores: [],
+        posicionMatrix: {
+          derecha: [0.5456, 0, 0, 0.5456, 389.9837, 450.4055],
+          izquierda: [1, 0, 0, 1, 702.6883, 583.8918],
+          centro: [1, 0, 0, 1, 579.3328, 562.0125]
+        }
+
+      },
+      {
+        idParte: "Short_número",
+        nombreMostrar: "Short número",
+        colores: [],
+        posicionMatrix: {}
+      },
+      {
+        idParte: "Remera_escudo",
+        nombreMostrar: "Escudo remera",
+        colores: ["#0E147C", "#0F3302", "#B2082E", "#F0F606"],
+        posicionMatrix: {
+          derecha: [0.5456, 0, 0, 0.5456, 389.9837, 450.4055],
+          izquierda: [1, 0, 0, 1, 702.6883, 583.8918],
+          centro: [0.5456, 0, 0, 0.5456, 562.7837,478.4709]
+        }
+      }
+    ],
   },
   {
     idModelo: 12,
